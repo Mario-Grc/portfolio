5b9e5e6cbfb9a07221a2c667a62cf689faf78572
Mario-Grc <mariotrabajos23@gmail.com>
2026-09-02 20:06:06 +0100

    chore: configure angular project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,23 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Contact } from './components/contact/contact';
+import { Experience } from './components/experience/experience';
+import { Footer } from './components/footer/footer';
+import { Hero } from './components/hero/hero';
+import { Navbar } from './components/navbar/navbar';
+import { Projects } from './components/projects/projects';
+import { Skills } from './components/skills/skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Navbar,
+    Hero,
+    Experience,
+    Projects,
+    Skills,
+    Contact,
+    Footer,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
