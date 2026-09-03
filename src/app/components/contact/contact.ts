fdd0f8434e29e8c5838838dc52a12e182d045f71
Mario-Grc <mariotrabajos23@gmail.com>
2026-09-03 22:09:30 +0100

    feat: add contact

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
-export class Contact {}
+export class Contact {
+  protected readonly email = 'mariogarciaabellan@gmail.com';
+  protected readonly githubUrl = 'https://github.com/Mario-Grc';
+  protected readonly linkedinUrl = 'https://www.linkedin.com/in/mario-garcia-abellan/';
+  protected readonly emailCopied = signal(false);
+
+  protected async copyEmail(): Promise<void> {
+    await navigator.clipboard.writeText(this.email);
+    this.emailCopied.set(true);
+    setTimeout(() => this.emailCopied.set(false), 2000);
+  }
+}
